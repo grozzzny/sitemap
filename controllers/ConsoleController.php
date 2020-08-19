8e3c2cbc1ffc01a41dd352984b72a11f14785cc6
grozzzny <grozzzny@list.ru>
2020-08-19 19:19:38 +0200

    v2

--- controllers/ConsoleController.php
+++ controllers/ConsoleController.php
@@ -6,8 +6,6 @@ use grozzzny\sitemap\components\Sitemap;
 use grozzzny\sitemap\Module;
 use yii\console\Controller;
 use Yii;
-use yii\easyii2\components\FastModel;
-use yii\easyii2\modules\article\api\Article;
 use yii\helpers\FileHelper;
 
 class ConsoleController extends Controller
@@ -16,18 +14,20 @@ class ConsoleController extends Controller
     /**
      * @var Sitemap
      */
-    private $sitemap;
+    protected $sitemap;
+
+    public $lastmodStaticPage = ''; // Y-m-d
 
     /**
      * @var Module
      */
     public $module;
 
-    private $sitemap_index = '/sitemap.xml';
+    public $sitemap_index = '/sitemap.xml';
 
-    private $sitemap_pages = '/sitemaps/sitemap_pages.xml';
+    public $sitemap_pages = '/sitemaps/sitemap_pages.xml';
 
-    private $sitemap_sections = '/sitemaps/sitemap_sections.xml';
+    public $sitemap_sections = '/sitemaps/sitemap_sections.xml';
 
     public $data_sitemap;
 
@@ -89,17 +89,12 @@ class ConsoleController extends Controller
 
         $this->install();
 
-        /** @var \grozzzny\sitemap\models\Sitemap $model */
-        $model = $this->module->getModelBySlug('sitemap');
-
-        foreach ($model::find()->where(['status' => FastModel::STATUS_ON])->orderBy(['lastmod' => SORT_ASC])->All() as $model){
-            $this->sitemap->insertSitemap($this->sitemap_pages, $model->loc, $model->getLastmod(), $model->changefreq, $model->priority);
+        foreach ($this->staticPages as $pages){
+            $this->sitemap->insertSitemap($this->sitemap_pages, $pages['loc'], $pages['lastmod'], $pages['changefreq'], $pages['priority']);
         }
 
-        $this->sitemap->updateSitemapIndex($this->sitemap_pages, $this->sitemap_index, $model->getLastmod());
+        $this->sitemap->updateSitemapIndex($this->sitemap_pages, $this->sitemap_index, $this->lastmodStaticPage);
     }
-
-
 
     public function actionUpdate()
     {
@@ -134,135 +129,5 @@ class ConsoleController extends Controller
     public function getDataSitemap()
     {
 //        $this->getDataArticles();
-//        $this->getDataCategories();
-//        $this->getDataGallery();
     }
-
-
-    public function getDataCategories()
-    {
-        $key_section = 'uslugi';
-        $categories = \yii\easyii2\modules\article\models\Category::find()->where([
-            'AND',
-            ['tree' => '2'],
-            ['!=', 'category_id', '2']
-        ])->all();
-
-        foreach($categories as $category){
-            if(!count($category->items)){
-                $loc = $this->module->domain . '/uslugi/'.$category->slug;
-                $changefreq = 'monthly';
-                $priority = '0.7';
-                $lastmod  = $this->lastmod;
-                echo $loc . PHP_EOL;
-                $this->data_sitemap[$key_section][] = array(
-                    'loc'           => $loc, //http://www.example.com/
-                    'lastmod'       => $lastmod, //2005-01-01
-                    'changefreq'    => $changefreq, //always hourly daily weekly monthly yearly never
-                    'priority'      => $priority, //0.8
-                );
-            }else{
-                $items = [];
-                foreach($category->items as $child){
-                    $loc = $this->module->domain . '/uslugi/'.$category->slug.'/'.$child->slug;
-                    $changefreq = 'monthly';
-                    $priority = '0.7';
-                    $lastmod  = $this->lastmod;
-                    echo $loc . PHP_EOL;
-                    $this->data_sitemap[$key_section][] = array(
-                        'loc'           => $loc, //http://www.example.com/
-                        'lastmod'       => $lastmod, //2005-01-01
-                        'changefreq'    => $changefreq, //always hourly daily weekly monthly yearly never
-                        'priority'      => $priority, //0.8
-                    );
-                }
-
-                $loc = $this->module->domain . '/uslugi/'.$category->slug;
-                $changefreq = 'monthly';
-                $priority = '0.7';
-                $lastmod  = $this->lastmod;
-                echo $loc . PHP_EOL;
-                $this->data_sitemap[$key_section][] = array(
-                    'loc'           => $loc, //http://www.example.com/
-                    'lastmod'       => $lastmod, //2005-01-01
-                    'changefreq'    => $changefreq, //always hourly daily weekly monthly yearly never
-                    'priority'      => $priority, //0.8
-                );
-            }
-        }
-    }
-
-
-    public function getDataArticles()
-    {
-        foreach(Article::cat('clinika')->items() as $article)
-        {
-
-            $key_section = 'clinika';
-
-            $loc = $this->module->domain . '/clinika/'.$article->slug;
-            $changefreq = 'monthly';
-            $priority = '0.7';
-            $lastmod  = date('Y-m-d', $article->time);
-
-            echo $loc . PHP_EOL;
-
-            $this->data_sitemap[$key_section][] = array(
-                'loc'           => $loc, //http://www.example.com/
-                'lastmod'       => $lastmod, //2005-01-01
-                'changefreq'    => $changefreq, //always hourly daily weekly monthly yearly never
-                'priority'      => $priority, //0.8
-            );
-        }
-    }
-
-    public function getDataGallery()
-    {
-        $items_gallery = [];
-        $key_section = 'gallery';
-
-        foreach(\yii\easyii2\modules\gallery\models\Category::tree() as $category){
-            $children = $category->children;
-
-            if(count($children) < 1){
-                $loc = $this->module->domain . '/foto/'.$category->slug;
-                $changefreq = 'monthly';
-                $priority = '0.7';
-                $lastmod  = date('Y-m-d', $category->time);
-            }elseif(count($children) == 1){
-                $loc = $this->module->domain . '/foto/'.$category->children[0]->slug;
-                $changefreq = 'monthly';
-                $priority = '0.7';
-                $lastmod  = date('Y-m-d', $category->children[0]->time);
-            }else{
-                $items = [];
-                foreach($children as $child){
-                    $loc = $this->module->domain . '/foto/'.$child->slug;
-                    $changefreq = 'monthly';
-                    $priority = '0.7';
-                    $lastmod  = date('Y-m-d', $child->time);
-
-                    $this->data_sitemap[$key_section][] = array(
-                        'loc'           => $loc, //http://www.example.com/
-                        'lastmod'       => $lastmod, //2005-01-01
-                        'changefreq'    => $changefreq, //always hourly daily weekly monthly yearly never
-                        'priority'      => $priority, //0.8
-                    );
-                }
-
-                $loc = $this->module->domain . '/foto/'.$children[0]->slug;
-                $changefreq = 'monthly';
-                $priority = '0.7';
-                $lastmod  = date('Y-m-d', $children[0]->time);
-            }
-
-            $this->data_sitemap[$key_section][] = array(
-                'loc'           => $loc, //http://www.example.com/
-                'lastmod'       => $lastmod, //2005-01-01
-                'changefreq'    => $changefreq, //always hourly daily weekly monthly yearly never
-                'priority'      => $priority, //0.8
-            );
-        }
-    }
-
 }
